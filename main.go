545ee886ed2ff9166ac597daedc3d8e8fea6aef6
depengli <ldpfieldlee@gmail.com>
2018-05-21 12:08:26 +0800

    fix init

--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
+	"jiakechaincode/control"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
-type ProductTrace struct {
-}
-
 func main() {
-	err := shim.Start(new(ProductTrace))
+	err := shim.Start(new(control.ProductTrace))
 	if err != nil {
 		fmt.Printf("Error starting ProductTrace: %s", err)
 	}
